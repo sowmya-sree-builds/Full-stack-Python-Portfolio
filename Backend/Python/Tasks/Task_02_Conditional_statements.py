f6930d26a6d615440853fdf44051c1133114eb04
sweetblink <its.sowmyasree@gmail.com>
2025-12-14 12:06:17 +0530

    Reuploaded Task 2 on conditional statements

---
+++ Backend/Python/Tasks/Task_02_Conditional_statements.py
@@ -0,0 +1,63 @@
+# Check if a number is divisible by both 3 and 5.
+
+# n = int(input("Enter your desired number:"))
+# if n % 3 == 0 and n % 5 == 0:
+#     print(f'{n} is divisible by both 3 and 5')
+# else:
+#     print(f'{n} is not divisible by both 3 and 5')
+
+
+# Compare two numbers and print which one is greater.
+
+
+# a = int(input('enter your first number:'))
+# b = int(input('enter your second number:'))
+# if a >b:
+#     print(f'{a} is greater than {b}')
+# else:
+#     print(f'{a} is less than {b}')
+
+
+# Ask for a score and print Grade A/B/C/Fail.
+# marks = int(input('Enter your score to find your grade:'))
+
+
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 75:
+#     print("Grade B")
+# elif marks >= 60:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+
+# Check if a number is positive and even.
+
+# n = int(input('Enter your number:'))
+# if n > 0 and n % 2 == 0:
+#     print(f'{n} is positive and even')
+# elif n > 0 and n % 2 != 0:
+#     print(f'{n} is positive and odd')
+# elif n < 0 and n % 2 == 0:
+#     print(f'{n} is negative and even')
+# elif n < 0 and n % 2 != 0:
+#     print(f'{n} is negative and odd')
+# else:
+#     print('The number is zero, which is even but not positive or negative')
+
+
+# Ask the users age and suggest:
+# Child if < 13
+# Teen if 13-19
+# Adult if 20+
+
+# age = int(input('Enter your age:'))
+# if age < 13:
+#     print('You are a child')
+# elif age > 13 and age < 19:
+#     print('You are a teen')
+# if age >= 20:
+#     print('you are an adult')
+# else:
+#     print('You are not human')
